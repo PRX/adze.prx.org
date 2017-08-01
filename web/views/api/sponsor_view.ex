558832d33842018693eb6a03e9a339d8bf532ac4
gcampo88 <gigi.campo@gmail.com>
2017-08-01 12:50:46 -0400

    fix sponsor views

--- web/views/api/sponsor_view.ex
+++ web/views/api/sponsor_view.ex
@@ -1,21 +1,43 @@
+require IEx
+
 defmodule Adze.API.SponsorView do
   use Adze.Web, :view
 
-  def render("index.json", %{sponsors: sponsors}) do
-    %{data: render_many(sponsors, Adze.API.SponsorView, "sponsor.json")}
+  def render("index.json", %{conn: conn, sponsors: sponsors}) do
+    %{
+      count: length(sponsors),
+      total: length(sponsors),
+      _embedded: %{
+        "prx:items": Enum.map(sponsors, fn(s) -> sponsor_json(s, conn) end)
+      },
+    }
   end
 
-  def render("show.json", %{sponsor: sponsor}) do
-     %{data: render_one(sponsor, Adze.API.SponsorView, "sponsor.json")}
-    # sponsor
-    # |> Map.put(:campaigns, data: sponsor.campaigns)
-    # |> render_one(sponsor, Adze.API.SponsorView, "sponsor.json")
+  def render("show.json", %{conn: conn, sponsor: sponsor}) do
+    campaigns = Enum.map(sponsor.campaigns, fn c ->
+      %{start_date: c.start_date, end_date: c.end_date}
+    end)
+    sponsor_json(sponsor, conn)
+    |> Map.put(:campaigns, campaigns)
   end
 
-  def render("sponsor.json", %{sponsor: sponsor}) do
-    %{id: sponsor.id,
+  defp sponsor_json(sponsor, conn) do
+    %{
+      id: sponsor.id,
       name: sponsor.name,
       billing_info: sponsor.billing_info,
-      notes: sponsor.notes}
+      notes: sponsor.notes,
+      _links: %{
+        self: %{
+          href: api_sponsor_path(conn, :show, sponsor),
+          templated: true
+        },
+        "prx:campaigns": %{
+          href: api_sponsor_campaign_path(conn, :index, sponsor),
+          templated: true
+        }
+      }
+    }
   end
+
 end
