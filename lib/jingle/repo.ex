0f9a65daa17028b35bc8d10c8219db37f048a91f
cavis <ryancavis@gmail.com>
2017-10-31 15:01:02 -0600

    Only load dotenv in dev/test

--- lib/jingle/repo.ex
+++ lib/jingle/repo.ex
@@ -2,15 +2,20 @@ defmodule Jingle.Repo do
   use Ecto.Repo, otp_app: :jingle
 
    def init(_, opts) do
-     Dotenv.load!
-     opts =
-       opts
-       |> Keyword.put(:database, Env.get(:db_env_postgres_database))
-       |> Keyword.put(:username, Env.get(:db_env_postgres_user))
-       |> Keyword.put(:password, Env.get(:db_env_postgres_password))
-       |> Keyword.put(:hostname, Env.get(:db_port_5432_tcp_addr))
-       |> Keyword.put(:port, Env.get(:db_port_5432_tcp_port))
-       |> Keyword.put(:pool_size, Env.get(:db_pool_size))
-     {:ok, opts}
+     {:ok, load_opts(opts, Mix.env)}
+  end
+
+  defp load_opts(opts, env) when env in [:dev, :test] do
+    Dotenv.load!
+    load_opts(opts, nil)
+  end
+  defp load_opts(opts, _env) do
+    opts
+    |> Keyword.put(:database, Env.get(:db_env_postgres_database))
+    |> Keyword.put(:username, Env.get(:db_env_postgres_user))
+    |> Keyword.put(:password, Env.get(:db_env_postgres_password))
+    |> Keyword.put(:hostname, Env.get(:db_port_5432_tcp_addr))
+    |> Keyword.put(:port, Env.get(:db_port_5432_tcp_port))
+    |> Keyword.put(:pool_size, Env.get(:db_pool_size))
   end
 end
