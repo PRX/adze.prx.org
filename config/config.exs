3d261b430c88be18401aca22372fa4c5be970bc2
cavis <ryancavis@gmail.com>
2017-10-31 14:28:44 -0600

    Dynamic db configs

--- config/config.exs
+++ config/config.exs
@@ -18,19 +18,18 @@ config :jingle, Jingle.Endpoint,
            adapter: Phoenix.PubSub.PG2]
 
 # Configures the repository
-config :jingle, Jingle.Repo,
- adapter: Ecto.Adapters.Postgres,
- database: System.get_env("DB_ENV_POSTGRES_DATABASE") || "${DB_ENV_POSTGRES_DATABASE}",
- username: System.get_env("DB_ENV_POSTGRES_USER") || "${DB_ENV_POSTGRES_USER}",
- password: System.get_env("DB_ENV_POSTGRES_PASSWORD") || "${DB_ENV_POSTGRES_PASSWORD}",
- hostname: System.get_env("DB_PORT_5432_TCP_ADDR") || "${DB_PORT_5432_TCP_ADDR}",
- port: System.get_env("DB_PORT_5432_TCP_PORT") || "${DB_PORT_5432_TCP_PORT}",
- pool_size: System.get_env("DB_POOL_SIZE") || "${DB_POOL_SIZE}"
+config :jingle, Jingle.Repo, adapter: Ecto.Adapters.Postgres
 
- # Environment config (precompiled OR from env variables)
- # MUST release with RELX_REPLACE_OS_VARS=true
+# Environment config (precompiled OR from env variables)
+# MUST release with RELX_REPLACE_OS_VARS=true
 config :jingle, :env_config,
-  id_host: System.get_env("ID_HOST") || "${ID_HOST}"
+  id_host: System.get_env("ID_HOST") || "${ID_HOST}",
+  db_env_postgres_database: System.get_env("DB_ENV_POSTGRES_DATABASE") || "${DB_ENV_POSTGRES_DATABASE}",
+  db_env_postgres_user: System.get_env("DB_ENV_POSTGRES_USER") || "${DB_ENV_POSTGRES_USER}",
+  db_env_postgres_password: System.get_env("DB_ENV_POSTGRES_PASSWORD") || "${DB_ENV_POSTGRES_PASSWORD}",
+  db_port_5432_tcp_addr: System.get_env("DB_PORT_5432_TCP_ADDR") || "${DB_PORT_5432_TCP_ADDR}",
+  db_port_5432_tcp_port: System.get_env("DB_PORT_5432_TCP_PORT") || "${DB_PORT_5432_TCP_PORT}",
+  db_pool_size: System.get_env("DB_POOL_SIZE") || "${DB_POOL_SIZE}"
 
 # Configures Elixir's Logger
 config :logger, :console,
--- lib/jingle/repo.ex
+++ lib/jingle/repo.ex
@@ -1,3 +1,16 @@
 defmodule Jingle.Repo do
   use Ecto.Repo, otp_app: :jingle
+
+   def init(_, opts) do
+     Dotenv.load!
+     opts =
+       opts
+       |> Keyword.put(:database, Env.get(:db_env_postgres_database))
+       |> Keyword.put(:username, Env.get(:db_env_postgres_user))
+       |> Keyword.put(:password, Env.get(:db_env_postgres_password))
+       |> Keyword.put(:hostname, Env.get(:db_port_5432_tcp_addr))
+       |> Keyword.put(:port, Env.get(:db_port_5432_tcp_port))
+       |> Keyword.put(:pool_size, Env.get(:db_pool_size))
+     {:ok, opts}
+  end
 end
