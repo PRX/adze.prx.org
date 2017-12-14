37a2fc98f8218c3e0023da0c29e4a49f35942df3
gcampo88 <gigi.campo@gmail.com>
2017-12-14 14:57:03 -0500

    use utc for timestamps

---
+++ priv/repo/migrations/20171214195245_use_utc_for_timestamps.exs
@@ -0,0 +1,26 @@
+defmodule Jingle.Repo.Migrations.UseUtcForTimestamps do
+  use Ecto.Migration
+
+  def change do
+    alter table(:campaigns) do
+      modify :inserted_at, :utc_datetime
+      modify :updated_at, :utc_datetime
+    end
+
+    alter table(:sponsors) do
+      modify :inserted_at, :utc_datetime
+      modify :updated_at, :utc_datetime
+    end
+
+    alter table(:creatives) do
+      modify :inserted_at, :utc_datetime
+      modify :updated_at, :utc_datetime
+    end
+
+    alter table(:podcasts) do
+      modify :inserted_at, :utc_datetime
+      modify :updated_at, :utc_datetime
+    end
+
+  end
+end
