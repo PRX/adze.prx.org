f4c65911a4292d6ca9bd20c4c1b8ab063f8932ec
gcampo88 <gigi.campo@gmail.com>
2017-12-15 13:31:40 -0500

    add hours and minutes to the dates that will be saved as UTC

--- test/models/campaign_test.exs
+++ test/models/campaign_test.exs
@@ -3,12 +3,26 @@ defmodule Jingle.CampaignTest do
 
   alias Jingle.Campaign
 
-  @valid_attrs %{original_copy: "some content", end_date: %{day: 17, month: 4, year: 2010}, podcast_id: 42, sponsor_id: 42, start_date: %{day: 17, month: 4, year: 2010}, zone: "some content"}
+  @valid_attrs %{original_copy: "some content", end_date: %{day: 17, month: 4, year: 2010}, podcast_id: 42, sponsor_id: 42, start_date: %{day: 17, month: 4, year: 2010}, due_date: %{day: 17, month: 4, year: 2010}, zone: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = Campaign.changeset(%Campaign{}, @valid_attrs)
     assert changeset.valid?
+  end
+
+  test "casts times to utc-convertable times without overwriting fields" do
+    changeset = Campaign.changeset(%Campaign{}, @valid_attrs)
+    assert changeset.valid?
+    assert changeset.changes.due_date.hour == 0
+
+    date_with_time = %{day: 17, month: 4, year: 2010, minute: 30, hour: 10}
+    new_params = %{@valid_attrs | end_date: date_with_time}
+
+    changeset = Campaign.changeset(%Campaign{}, new_params)
+    assert changeset.valid?
+    assert changeset.changes.end_date.hour == 10
+    assert changeset.changes.end_date.minute == 30
   end
 
   test "changeset with invalid attributes" do
--- web/models/campaign.ex
+++ web/models/campaign.ex
@@ -2,8 +2,8 @@ defmodule Jingle.Campaign do
   use Jingle.Web, :model
 
   schema "campaigns" do
-    field :start_date, :datetime
-    field :end_date, :datetime
+    field :start_date, :utc_datetime
+    field :end_date, :utc_datetime
     field :due_date, :utc_datetime
     field :original_copy, :string
     field :edited_copy, :string
@@ -23,9 +23,22 @@ defmodule Jingle.Campaign do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:sponsor_id, :podcast_id, :start_date, :end_date, :due_date, :original_copy, :edited_copy, :must_say, :notes, :approved, :zone])
-    |> validate_required([:start_date, :end_date, :original_copy, :zone])
+    |> cast(utc_compatible_dates(params), [:sponsor_id, :podcast_id, :start_date, :end_date, :due_date, :original_copy, :edited_copy, :must_say, :notes, :approved, :zone])
+    |> validate_required([:start_date, :end_date, :due_date, :original_copy, :zone])
     |> assoc_constraint(:sponsor)
     |> assoc_constraint(:podcast)
+  end
+
+  defp utc_compatible_dates(params) do
+    midnight = %{hour: 0, minute: 0}
+
+    try do
+      Map.update!(params, :start_date, &Map.merge(midnight, &1))
+      |> Map.update!(:end_date, &Map.merge(midnight, &1))
+      |> Map.update!(:due_date, &Map.merge(midnight, &1))
+    rescue
+      # if one of these is missing, let the requirement validation catch it
+      KeyError -> params
+    end
   end
 end
